5f5fe1d0479746e04533800b2b8be8b7d08246d1
AlexMontoyaG <168393124+AlexMontoyaG@users.noreply.github.com>
2025-01-05 01:01:46 -0500

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
 export const MIN_WLD = 3;
 export const WALLET_ADDRESS = '0x32738053b17601aba6d6941e4c870129f3c4f371';
-export const COMMISSION = 4000; // Comisión fija en COP
+export const COMMISSION = 3500; // Comisión fija en COP
